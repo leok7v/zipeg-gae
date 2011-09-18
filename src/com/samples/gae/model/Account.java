538a7977f00e9e49d86708238ee6f46b39bf5ea6
leo.kuznetsov@gmail.com <leo.kuznetsov@gmail.com@17460834-a702-6099-db79-2fc3406ef28e>
2011-09-18 08:52:29 +0000

    Accounts and PersistenceManager

---
+++ src/com/samples/gae/model/Account.java
@@ -0,0 +1,31 @@
+package com.samples.gae.model;
+
+import static com.zipeg.gae.util.*;
+import javax.jdo.annotations.*;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Account {
+
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	@PrimaryKey public String id;
+    @Persistent public String nickname;
+    @Persistent public String email;
+    @Persistent public String federatedIdentity;
+    @Persistent public String authDomain;
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (o instanceof Account) {
+            Account a = (Account)o;
+            return  equal(id, a.id) &&
+                    equal(nickname, a.nickname) &&
+                    equal(email, a.email) &&
+                    equal(federatedIdentity, a.federatedIdentity) &&
+                    equal(authDomain, a.authDomain);
+        } else {
+            return false;
+        }
+    }
+
+}
