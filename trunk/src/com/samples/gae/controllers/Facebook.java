d817c8cc995c0c9017e2ca08bbea999042b13871
leo.kuznetsov@gmail.com <leo.kuznetsov@gmail.com@17460834-a702-6099-db79-2fc3406ef28e>
2011-09-18 22:45:59 +0000

    Facebook fluid canvas

---
+++ trunk/src/com/samples/gae/controllers/Facebook.java
@@ -0,0 +1,14 @@
+package com.samples.gae.controllers;
+
+import com.zipeg.gae.*;
+
+public class Facebook extends Context {
+
+    public void fbApp() {
+        appendToHead("<style type=\"text/css\">" +
+                "html,body{margin:0;padding:0;border:0;background-color:#FF0000;" +
+                "height: 100%;min-height: 100%;}" +
+                "</style>");
+    }
+
+}
